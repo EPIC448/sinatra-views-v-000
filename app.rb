f85dfd188d2381e1c4c32f85ac6ecf977e8b6caa
Sam. O <samrey2018@gmail.com>
2018-04-03 20:48:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-		get '/' do    # Note   This are call, routes.
+		get '/' do    # Note   This is the way we call routes in Sinatra.
 	     erb :index
 		end
 
